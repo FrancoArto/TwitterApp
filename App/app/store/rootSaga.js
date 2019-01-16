a267a90205f4108b9137e7fbe310920f2fe020fc
Franco Uriel Arto Vazquez <franco.arto@globant.com>
2019-01-16 17:04:49 -0300

    Removed notificationsSaga

--- App/app/store/rootSaga.js
+++ App/app/store/rootSaga.js
@@ -2,13 +2,11 @@ import { all } from 'redux-saga/effects'
 import {trendsSaga} from './trends/trendsSaga'
 import { tweetsSaga } from './tweets/tweetsSaga';
 import { userSaga } from './users/userSaga';
-import { notificationsSaga } from './notifications/notificationsSaga';
 
 export default function* rootSaga() {
   yield all([
     tweetsSaga(),
     trendsSaga(),
-    userSaga(),
-    notificationsSaga()
+    userSaga()
   ])
 }
